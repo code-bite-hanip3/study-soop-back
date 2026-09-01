21f563e59668168e12aa6ee30c8b889a5121664e
ha kyeong <hess93hess93@gmail.com>
2026-09-02 08:35:04 +0900

    remove: cors 삭제

--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import { config } from './config/config.js';
-import { cors } from './middlewares/cors.js';
 
 const app = express();
 
-app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
